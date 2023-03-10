53a72c969c356e5a8615f616eeb89f86eaabb9de
Lyo-T <lyotereom>
2023-03-10 15:52:43 +0000

    updated db env var

--- startup/db.js
+++ startup/db.js
@@ -2,8 +2,15 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
+if (!process.env.MONGO_URL) {
+    throw new Error("Please add the MONGO_URL environment variable");
+  }
+
 module.exports = function(){
-    const db = config.get('db');
-    mongoose.connect(db)
+    const db = config.get('MONGO_URL');
+    mongoose.connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
         .then(() => winston.info(`connected to ${db}`));
 }
